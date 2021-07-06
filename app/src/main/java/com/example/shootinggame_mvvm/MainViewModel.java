89540911090ea0f1c6fae15b53368b4220242b27
Lee Sumin <sumini0715@naver.com>
2021-07-06 17:07:27 +0900

    add ViewModel

--- app/src/main/java/com/example/shootinggame_mvvm/MainViewModel.java
+++ app/src/main/java/com/example/shootinggame_mvvm/MainViewModel.java
@@ -1,8 +1,27 @@
 package com.example.shootinggame_mvvm;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.shootinggame_mvvm.Model.Bullet;
+import com.example.shootinggame_mvvm.Model.Enemy;
+import com.example.shootinggame_mvvm.Model.StepInfo;
+
+import java.util.HashMap;
+
 public class MainViewModel extends ViewModel {
+    private static MutableLiveData<StepInfo> stepInfoData = new MutableLiveData<>();
+
+    public MutableLiveData<StepInfo> getStepInfoData() {
+        if(stepInfoData == null) {
+            stepInfoData = new MutableLiveData<>();
+        }
+        return stepInfoData;
+    }
+
+    public void setStepInfoData(StepInfo data) {
+        stepInfoData.setValue(data);
+    }
 
 }
